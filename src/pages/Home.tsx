0b1323cdb0d53706fbdcfbf0544ae252b4b4b247
Luana Oliveira <luolivierdesigner@gmail.com>
2026-02-28 13:22:12 -0300

    setup inicial do projeto com vite, typescript e sass

---
+++ src/pages/Home.tsx
@@ -0,0 +1,8 @@
+export function Home() {
+    return (
+        <div>
+            <h1>Assistente de Mercado</h1>
+            <p>Monitorando ativos...</p>
+        </div>
+    )
+}
---
+++ src/types/Product.ts
@@ -0,0 +1,7 @@
+export interface Product {
+    id?: number
+    productName: string
+    descriptionShort: string
+    photo: string
+    price: number
+}
